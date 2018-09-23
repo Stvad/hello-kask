23761f4d7ca89f6c16f8d61bf0d848a845d4ef85
Vladyslav Sitalo <sitalov@amazon.com>
2018-09-23 15:23:08 +0100

    Use new Java-friendly overloads for handler function.

--- src/main/java/org/stvad/kask/example/JavaFunctionalHandlers.java
+++ src/main/java/org/stvad/kask/example/JavaFunctionalHandlers.java
@@ -3,16 +3,27 @@ package org.stvad.kask.example;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.LaunchRequest;
 import kotlin.Unit;
-import kotlin.reflect.KClass;
 
-import static kotlin.jvm.internal.Reflection.getOrCreateKotlinClass;
 import static org.stvad.kask.request.HandlersKt.handle;
 import static org.stvad.kask.request.HandlersKt.respond;
 
 public class JavaFunctionalHandlers {
-    public static final RequestHandler helloIntentHandler = handle(
-            new String[]{"HelloWorldIntent"},
-            input -> input.getResponseBuilder().withSpeech("Hello World!").build());
+    public static final RequestHandler helloIntentHandler =
+            handle(input -> input.getResponseBuilder()
+                            .withSpeech("Hello World!")
+                            .build(),
+                    "HelloWorldIntent");
+
+    public static final RequestHandler unusedLaunchRequestHandler =
+            handle(input -> {
+                        String welcomeSpeech = "Hey There!";
+                        return input.getResponseBuilder()
+                                .withSpeech(welcomeSpeech)
+                                .withReprompt(welcomeSpeech)
+                                .withSimpleCard("Hello Kask", welcomeSpeech)
+                                .build();
+                    },
+                    LaunchRequest.class);
 
     public static final RequestHandler awkwardStopIntentHandler = respond(new String[]{"AMAZON.StopIntent"},
             (builder, i) -> {
@@ -20,9 +31,4 @@ public class JavaFunctionalHandlers {
                 return Unit.INSTANCE;
             });//this is too awkward and there is not much point given the lack of implicit context =\
 
-    public static final RequestHandler unusedLaunchRequestHandler = handle(
-            new KClass[]{getOrCreateKotlinClass(LaunchRequest.class)},
-            (input -> input.getResponseBuilder()
-                    .withSpeech("Hey There!")
-                    .build()));
 }
